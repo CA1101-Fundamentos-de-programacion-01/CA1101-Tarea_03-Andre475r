21a8e2734b9f1a60891cb5521f82c6fcd62499a2
Andre475r <98907301+Andre475r@users.noreply.github.com>
2022-02-17 21:44:46 -0600

    Update ejercicio_02.py

--- ejercicio_02.py
+++ ejercicio_02.py
@@ -1,7 +1,11 @@
 
 def calcular_litros_por_hora(tiempo_de_ciclismo)(3):
+  print(3)
 def calcular_litros_por_hora(tiempo_de_ciclismo)(6.7):
+  (6.7)
+  
 def calcular_litros_por_hora(tiempo_de_ciclismo)(11.8):
+  print(11.8)
 
 
 
